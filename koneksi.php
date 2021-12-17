08ad7caec72c85494791f176f8481d5bf8af7286
nesviramahdani <63386233+nesviramahdani@users.noreply.github.com>
2021-12-17 21:16:24 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "dblaundry";
+$host = "db4free.net";
+$user = "galonly";
+$pass = "12345678";
+$db   = "galonly";
 
 $konek = mysqli_connect($host, $user, $pass, $db) or die("Database MYSQL Tidak Terhubung");
